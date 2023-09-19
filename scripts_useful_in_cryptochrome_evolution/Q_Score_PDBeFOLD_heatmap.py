aa08f575cb23661eb06b490563342198af0ca80c
sayani <70441269+saysayani11@users.noreply.github.com>
2023-09-19 16:02:46 +0530

    Update Q_Score_PDBeFOLD_heatmap.py

--- scripts_useful_in_cryptochrome_evolution/Q_Score_PDBeFOLD_heatmap.py
+++ scripts_useful_in_cryptochrome_evolution/Q_Score_PDBeFOLD_heatmap.py
@@ -1,31 +1,21 @@
-import os
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import os, pandas as pd, seaborn as sns, matplotlib.pyplot as plt
+import numpy as np
 
-os.chdir(r"C:\Users\saySa\OneDrive\Desktop\task_1\PDBeFOLD")
-
+os.chdir(r"C:\Users\saySa\OneDrive\Desktop\task_1\PDBeFOLD\RMSD")
 df = pd.read_excel('rmsd_pdbefold.xlsx')
 
-# Extracting the PDB IDs for rows and columns
-row_labels = df.iloc[1:, 1].values
-row_labels = [label.replace('.pdb:A', '') for label in row_labels]
+row_labels = [x.replace('.pdb:A', '') for x in df.iloc[1:, 1]]
+col_labels = [x.replace('.pdb:A', '') for x in df.iloc[0, 2:]]
 
-col_labels = df.iloc[0, 2:].values
-col_labels = [label.replace('.pdb:A', '') for label in col_labels]
+df_mod = df.iloc[1:, 2:].fillna(1).applymap(lambda x: pd.to_numeric(str(x).strip()))
+df_mod.columns, df_mod.index = col_labels, row_labels
 
-df_modified = df.iloc[1:, 2:]
-df_modified.fillna(1, inplace=True)
-df_modified.columns = col_labels
-df_modified.index = row_labels
-
-# Strip whitespace and convert to numeric
-df_modified = df_modified.applymap(lambda x: pd.to_numeric(str(x).strip(), errors='coerce'))
+# Zero out the diagonal
+np.fill_diagonal(df_mod.values, np.nan)
 
 sns.set_style("white")
-plt.figure(figsize=(10, 8), dpi=300)  # Set DPI here
-sns.heatmap(df_modified, annot=True, cmap='RdBu', cbar_kws={"label": "RMSD Value"}, fmt='.2f')
+plt.figure(figsize=(10, 8), dpi=300)
+sns.heatmap(df_mod, annot=True, cmap='RdBu', cbar_kws={"label": "RMSD"}, fmt='.2f', mask=np.isnan(df_mod))
 plt.tight_layout()
 plt.savefig("heatmap.png", dpi=300)
-
 plt.show()
